5b6336c4414b1655deb866ea183f34cdbaa7ed28
Sonny Selten <shg.selten@student.han.nl>
2024-06-23 19:05:08 +0200

    pushing latest version to git

--- arduino_server/cserver.c
+++ arduino_server/cserver.c
@@ -25,20 +25,28 @@ void initializeBuffer(CircularBuffer *cb, int bufferSize)
 
 void addToBuffer(CircularBuffer *cb, int item)
 {
-  if (cb->count < cb->bufferSize)
-  {
-    cb->data[cb->tail] = item;
-    cb->tail = (cb->tail + 1) % cb->bufferSize;
-    cb->count++;
-  }
-  else
-  {
-    cb->data[cb->tail] = item;
-    cb->tail = (cb->tail + 1) % cb->bufferSize;
-    cb->head = (cb->head + 1) % cb->bufferSize;
-  }
-  printf("Added %d to buffer. Head: %d, Tail: %d, Count: %d\n", item, cb->head, cb->tail, cb->count);
+    if (cb->count < cb->bufferSize)
+    {
+        cb->data[cb->tail] = item;
+        cb->tail = (cb->tail + 1) % cb->bufferSize;
+        cb->count++;
+    }
+    else
+    {
+        cb->data[cb->tail] = item;
+        cb->tail = (cb->tail + 1) % cb->bufferSize;
+        cb->head = (cb->head + 1) % cb->bufferSize;
+    }
+    printf("Added %d to buffer. Head: %d, Tail: %d, Count: %d\n", item, cb->head, cb->tail, cb->count);
+    printf("Current buffer state: ");
+    for (int i = 0; i < cb->count; i++)
+    {
+        int value = cb->data[(cb->head + i) % cb->bufferSize];
+        printf("%d ", value);
+    }
+    printf("\n");
 }
+
 
 int pullFromBuffer(CircularBuffer *cb)
 {
@@ -55,19 +63,22 @@ int pullFromBuffer(CircularBuffer *cb)
 
 double getBufferAverage(CircularBuffer *buffer)
 {
-  if (buffer->count == 0)
-  {
-    return -1.0; // Return -1.0 if the buffer is empty
-  }
+    if (buffer->count == 0)
+    {
+        return -1.0; // Return -1.0 if the buffer is empty
+    }
 
-  double sum = 0;
-  for (int i = 0; i < buffer->count; i++)
-  {
-    int value = buffer->data[(buffer->head + i) % buffer->bufferSize];
-    sum += value;
-  }
-  return sum / buffer->count;
+    double sum = 0;
+    for (int i = 0; i < buffer->count; i++)
+    {
+        int value = buffer->data[(buffer->head + i) % buffer->bufferSize];
+        sum += value;
+    }
+    double average = sum / buffer->count;
+    printf("Calculated average: %f\n", average);
+    return average;
 }
+
 
 double getBufferStandardDeviation(CircularBuffer *buffer)
 {
@@ -117,140 +128,161 @@ double getBufferActual(CircularBuffer *buffer)
 
 void setBufferSize(CircularBuffer *buffer, int newSize)
 {
-  buffer->bufferSize = newSize;
-  buffer->data = (int *)realloc(buffer->data, newSize * sizeof(int));
-  buffer->head = 0;
-  buffer->tail = buffer->count % newSize;
-  if (buffer->count > newSize)
-  {
-    buffer->count = newSize;
-  }
-  printf("Buffer size set to: %d. Head: %d, Tail: %d, Count: %d\n", newSize, buffer->head, buffer->tail, buffer->count);
+    if (newSize <= 0)
+    {
+        printf("Invalid buffer size: %d\n", newSize);
+        return;
+    }
+
+    int *newData = (int *)malloc(newSize * sizeof(int));
+    int count = buffer->count < newSize ? buffer->count : newSize;
+
+    for (int i = 0; i < count; i++)
+    {
+        newData[i] = buffer->data[(buffer->head + i) % buffer->bufferSize];
+    }
+
+    free(buffer->data);
+    buffer->data = newData;
+    buffer->bufferSize = newSize;
+    buffer->count = count;
+    buffer->head = 0;
+    buffer->tail = count % newSize;
+
+    printf("Buffer size set to: %d. Head: %d, Tail: %d, Count: %d\n", newSize, buffer->head, buffer->tail, buffer->count);
+    printf("New buffer state: ");
+    for (int i = 0; i < buffer->count; i++)
+    {
+        printf("%d ", buffer->data[i]);
+    }
+    printf("\n");
 }
+
+
 
 struct response handleRequest(struct stream stream)
 {
-  printf("\n! Handling request : START\n");
+    printf("\n! Handling request : START\n");
 
-  char buffer[1024] = {0};
-  int bufferIndex = 0;
+    char buffer[1024] = {0};
+    int bufferIndex = 0;
 
-  while (stream.available() > 0 && bufferIndex < sizeof(buffer) - 1)
-  {
-    buffer[bufferIndex++] = stream.read();
-  }
-  buffer[bufferIndex] = '\0';
+    while (stream.available() > 0 && bufferIndex < sizeof(buffer) - 1)
+    {
+        buffer[bufferIndex++] = stream.read();
+    }
+    buffer[bufferIndex] = '\0';
 
-  // Print the received request
-  // printf("Received request: %s\n", buffer);
+    // Print the received request
+    // printf("Received request: %s\n", buffer);
 
-  // Read the request line
-  char *line = strtok(buffer, "\r\n");
-  char method[10], uri[50], protocol[10];
-  sscanf(line, "%s %s %s", method, uri, protocol);
+    // Read the request line
+    char *line = strtok(buffer, "\r\n");
+    char method[10], uri[50], protocol[10];
+    sscanf(line, "%s %s %s", method, uri, protocol);
 
-  // Print data
-  printf("Method: %s, URI: %s, Protocol: %s\n", method, uri, protocol);
+    // Print data
+    printf("Method: %s, URI: %s, Protocol: %s\n", method, uri, protocol);
 
-  char *header = strtok(NULL, "\r\n\r\n");
-  char *body = strtok(NULL, "\0");
-  int contentLength = header ? atoi(header + 16) : 0;
+    char *header = strtok(NULL, "\r\n\r\n");
+    char *body = strtok(NULL, "\0");
+    int contentLength = header ? atoi(header + 16) : 0;
 
-  printf("Header: %s\n", header);
-  printf("Content Length int value: %d\n", contentLength);
+    printf("Header: %s\n", header);
+    printf("Content Length int value: %d\n", contentLength);
 
-  if (strcmp(method, "PUT") == 0)
-  {
-    if (strcmp(uri, "/config/mode") == 0)
+    if (strcmp(method, "PUT") == 0)
     {
-      currentMode = (strcmp(body, "active") == 0) ? MODE_ACTIVE : MODE_PASSIVE;
-      printf("Set mode to %s\n", (currentMode == MODE_ACTIVE) ? "active" : "passive");
-      return (struct response){.code = CREATED_201_PUT_MODE_PASSIVE};
-    }
-    else if (strcmp(uri, "/config/cbuffsize") == 0)
-    {
-      int value = atoi(body); // Get the value from the body
-      printf("Value from body: %d\n", value);
+        if (strcmp(uri, "/config/mode") == 0)
+        {
+            currentMode = (strcmp(body, "active") == 0) ? MODE_ACTIVE : MODE_PASSIVE;
+            printf("Set mode to %s\n", (currentMode == MODE_ACTIVE) ? "active" : "passive");
+            return (struct response){.code = CREATED_201_PUT_MODE_PASSIVE};
+        }
+        else if (strcmp(uri, "/config/cbuffsize") == 0)
+        {
+            int value = atoi(body); // Get the value from the body
+            printf("Value from body: %d\n", value);
 
-      setBufferSize(&sensorBuffer1, value); // Example size
-      setBufferSize(&sensorBuffer2, value); // Example size
-      printf("Set circular buffer size to %d\n", value);
-      return (struct response){.code = CREATED_201_PUT_CBUFFSIZE};
+            setBufferSize(&sensorBuffer1, value); // Example size
+            setBufferSize(&sensorBuffer2, value); // Example size
+            printf("Set circular buffer size to %d\n", value);
+            return (struct response){.code = CREATED_201_PUT_CBUFFSIZE};
+        }
     }
-  }
-  else if (strcmp(method, "DELETE") == 0)
-  {
-    if (strncmp(uri, "/sensors/1", 10) == 0)
+    else if (strcmp(method, "DELETE") == 0)
     {
-      initializeBuffer(&sensorBuffer1, sensorBuffer1.bufferSize);
-      printf("Deleted measurements for sensor 1\n");
-      return (struct response){.code = CREATED_201_DELETE_MEASUREMENTS};
+        if (strncmp(uri, "/sensors/1", 10) == 0)
+        {
+            initializeBuffer(&sensorBuffer1, sensorBuffer1.bufferSize);
+            printf("Deleted measurements for sensor 1\n");
+            return (struct response){.code = CREATED_201_DELETE_MEASUREMENTS};
+        }
+        else if (strncmp(uri, "/sensors/2", 10) == 0)
+        {
+            initializeBuffer(&sensorBuffer2, sensorBuffer2.bufferSize);
+            printf("Deleted measurements for sensor 2\n");
+            return (struct response){.code = CREATED_201_DELETE_MEASUREMENTS};
+        }
     }
-    else if (strncmp(uri, "/sensors/2", 10) == 0)
+    else if (strcmp(method, "POST") == 0)
     {
-      initializeBuffer(&sensorBuffer2, sensorBuffer2.bufferSize);
-      printf("Deleted measurements for sensor 2\n");
-      return (struct response){.code = CREATED_201_DELETE_MEASUREMENTS};
-    }
-  }
-  else if (strcmp(method, "POST") == 0)
-  {
-    int value = atoi(body); // Get the value from the body
-    printf("Value from body: %d\n", value);
+        int value = atoi(body); // Get the value from the body
+        printf("Value from body: %d\n", value);
 
-    if (strncmp(uri, "/sensors/1", 10) == 0)
-    {
-      addToBuffer(&sensorBuffer1, value); // Use value from body
-      printf("Posted measurement %d to sensor 1\n", value);
-      return (struct response){.code = CREATED_201_POST_MEASUREMENT};
+        if (strncmp(uri, "/sensors/1", 10) == 0)
+        {
+            addToBuffer(&sensorBuffer1, value); // Use value from body
+            printf("Posted measurement %d to sensor 1\n", value);
+            return (struct response){.code = CREATED_201_POST_MEASUREMENT};
+        }
+        else if (strncmp(uri, "/sensors/2", 10) == 0)
+        {
+            addToBuffer(&sensorBuffer2, value); // Use value from body
+            printf("Posted measurement %d to sensor 2\n", value);
+            return (struct response){.code = CREATED_201_POST_MEASUREMENT};
+        }
     }
-    else if (strncmp(uri, "/sensors/2", 10) == 0)
+    else if (strcmp(method, "GET") == 0)
     {
-      addToBuffer(&sensorBuffer2, value); // Use value from body
-      printf("Posted measurement %d to sensor 2\n", value);
-      return (struct response){.code = CREATED_201_POST_MEASUREMENT};
+        if (strncmp(uri, "/sensors/1/avg", 14) == 0)
+        {
+            double avg = getBufferAverage(&sensorBuffer1);
+            printf("GET /sensors/1/avg: %.6f\n", avg); // Increased precision
+            return (struct response){.code = OK_200_GET_AVG, .get_avg = avg};
+        }
+        else if (strncmp(uri, "/sensors/1/stdev", 16) == 0)
+        {
+            double stdev = getBufferStandardDeviation(&sensorBuffer1);
+            printf("GET /sensors/1/stdev: %.6f\n", stdev); // Increased precision
+            return (struct response){.code = OK_200_GET_STDEV, .get_stdev = stdev};
+        }
+        else if (strncmp(uri, "/sensors/1/actual", 17) == 0)
+        {
+            double actual = getBufferActual(&sensorBuffer1);
+            printf("GET /sensors/1/actual: %.6f\n", actual); // Increased precision
+            return (struct response){.code = OK_200_GET_ACTUAL, .get_actual = actual};
+        }
+        else if (strncmp(uri, "/sensors/2/avg", 14) == 0)
+        {
+            double avg = getBufferAverage(&sensorBuffer2);
+            printf("GET /sensors/2/avg: %.6f\n", avg); // Increased precision
+            return (struct response){.code = OK_200_GET_AVG, .get_avg = avg};
+        }
+        else if (strncmp(uri, "/sensors/2/stdev", 16) == 0)
+        {
+            double stdev = getBufferStandardDeviation(&sensorBuffer2);
+            printf("GET /sensors/2/stdev: %.6f\n", stdev); // Increased precision
+            return (struct response){.code = OK_200_GET_STDEV, .get_stdev = stdev};
+        }
+        else if (strncmp(uri, "/sensors/2/actual", 17) == 0)
+        {
+            double actual = getBufferActual(&sensorBuffer2);
+            printf("GET /sensors/2/actual: %.6f\n", actual); // Increased precision
+            return (struct response){.code = OK_200_GET_ACTUAL, .get_actual = actual};
+        }
     }
-  }
-  else if (strcmp(method, "GET") == 0)
-  {
-    if (strncmp(uri, "/sensors/1/avg", 14) == 0)
-    {
-      double avg = getBufferAverage(&sensorBuffer1);
-      printf("GET /sensors/1/avg: %.6f\n", avg); // Increased precision
-      return (struct response){.code = OK_200_GET_AVG, .get_avg = avg};
-    }
-    else if (strncmp(uri, "/sensors/1/stdev", 16) == 0)
-    {
-      double stdev = getBufferStandardDeviation(&sensorBuffer1);
-      printf("GET /sensors/1/stdev: %.6f\n", stdev); // Increased precision
-      return (struct response){.code = OK_200_GET_STDEV, .get_stdev = stdev};
-    }
-    else if (strncmp(uri, "/sensors/1/actual", 17) == 0)
-    {
-      double actual = getBufferActual(&sensorBuffer1);
-      printf("GET /sensors/1/actual: %.6f\n", actual); // Increased precision
-      return (struct response){.code = OK_200_GET_ACTUAL, .get_actual = actual};
-    }
-    else if (strncmp(uri, "/sensors/2/avg", 14) == 0)
-    {
-      double avg = getBufferAverage(&sensorBuffer2);
-      printf("GET /sensors/2/avg: %.6f\n", avg); // Increased precision
-      return (struct response){.code = OK_200_GET_AVG, .get_avg = avg};
-    }
-    else if (strncmp(uri, "/sensors/2/stdev", 16) == 0)
-    {
-      double stdev = getBufferStandardDeviation(&sensorBuffer2);
-      printf("GET /sensors/2/stdev: %.6f\n", stdev); // Increased precision
-      return (struct response){.code = OK_200_GET_STDEV, .get_stdev = stdev};
-    }
-    else if (strncmp(uri, "/sensors/2/actual", 17) == 0)
-    {
-      double actual = getBufferActual(&sensorBuffer2);
-      printf("GET /sensors/2/actual: %.6f\n", actual); // Increased precision
-      return (struct response){.code = OK_200_GET_ACTUAL, .get_actual = actual};
-    }
-  }
 
-  printf("Returning 500 Internal Server Error\n");
-  return (struct response){.code = INTERNAL_SERVER_ERROR_500};
+    printf("Returning 500 Internal Server Error\n");
+    return (struct response){.code = INTERNAL_SERVER_ERROR_500};
 }
